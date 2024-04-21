54e25d9dde764313e5a6821c302dfea5c84775c0
Asrani-Aman <amanasrani1712@gmail.com>
2024-04-22 00:44:15 +0530

    add upload files page

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 import ClientSideProviderTest from "@/components/clientSideProviderTest";
-
+import { EdgeStoreProvider } from '@/lib/edgestore'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -19,11 +19,13 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {/* <ClientSideProviderTest> */}
+        <EdgeStoreProvider>
           <div className="container">
             <Navbar />
             {children}
             <Footer />
           </div>
+          </EdgeStoreProvider>
         {/* </ClientSideProviderTest> */}
       </body>
     </html>
--- src/app/upload/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>Radhe Radhe Govind Govind Radhe</div>
-  )
-}
-
-export default page
